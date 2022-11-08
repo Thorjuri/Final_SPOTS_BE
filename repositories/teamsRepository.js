fe0864d56bb6e9761e9a147a57bc993d471aa9f9
Thorjuri <sjl1992@naver.com>
2022-11-08 10:56:17 +0900

    11/8 서주리 teams 예외처리, wrapAsyncContoller 에러핸들링 추가, 코드 리팩토링:service 레이어로 코드 이동

--- repositories/teamsRepository.js
+++ repositories/teamsRepository.js
@@ -3,6 +3,23 @@ const { Reservations, Users, Teams } = require('../models');
 class TeamsRepository {
 
     
+    checkMyTeam = async(nickname)=> {
+        const data = await Users.findOne({
+            attributes: ['teamName'],
+            where: { nickname }
+        });
+        return data.teamName.team
+    };
+
+    getMember = async(teamName)=> {
+        const data = await Teams.findOne({
+            attributes: ['member', 'player'],
+            where: { teamName }
+        });
+        const players = data.player.player
+        const members = data.member
+        return {players, members};
+    }
 
     getMyTeam = async(userId)=> {
         const data = await Users.findOne({
@@ -28,54 +45,19 @@ class TeamsRepository {
         return {data : data, message: "신규 팀 생성 완료."};
     };
 
-    updateTeam = async(nickname, teamName)=> {
-        const data = await Users.findOne({  //users 테이블의 팀 목록에 추가 
-            attributes: ['teamName'],
-            where: { nickname }
-        });
-        const teamArr = data.teamName.team;
-        if(teamArr.indexOf(teamName)>= 0) { throw new Error('이미 가입된 팀 입니다.') };
-        teamArr.push(teamName);
-        const newTeam = {"team" : teamArr};
+    updateTeam = async(nickname, teamName, newTeam, newMember, newPlayer)=> {
         await Users.update({ teamName: newTeam }, { where: { nickname } });
-        
-        const players = await Teams.findOne({  //Teams 테이블의 해당 팀의 player, member에 추가
-            attributes: ['player', 'member'],
-            where: { teamName }
-        });
-        const playerArr = players.player.player;
-        playerArr.push(nickname);
-        const newPlayer = {"player" : playerArr};
-        const newMember = players.member + 1 ;
         await Teams.update({ player: newPlayer, member: newMember }, { where: { teamName } });
-        return {data : teamArr, team: playerArr, message: "팀 가입 완료."};
+
+        return {team : newTeam, player: newPlayer, message: "팀 가입 완료."};
     };
 
     
-    deleteTeam = async(nickname, teamName)=> {
-        const data = await Users.findOne({  //users 테이블의 팀 목록에서 삭제 
-            attributes: ['teamName'],
-            where: { nickname }
-        });
-        const teamArr = data.teamName.team;
-        if(teamArr.indexOf(teamName)<0) { throw new Error('가입되지 않은 팀 입니다.') };
-        const idx = teamArr.indexOf(teamName);
-        teamArr.splice(idx, 1);
-        const newTeam = {"team" : teamArr};
+    deleteTeam = async(nickname, teamName, newTeam, newMember, newPlayer)=> {
         await Users.update({ teamName: newTeam }, { where: { nickname } });
-
-        const players = await Teams.findOne({  //Teams 테이블의 해당 팀의 player, member에서 삭제
-            attributes: ['player', 'member'],
-            where: { teamName }
-        });
-        const playerArr = players.player.player;
-        const index = playerArr.indexOf(nickname);
-        playerArr.splice(index, 1);
-        const newPlayer = {"player" : playerArr};
-        const newMember = players.member -1 ;
         await Teams.update({ player: newPlayer, member: newMember }, { where: { teamName } });
 
-        return {data : teamArr, team: playerArr, message: "팀 탈퇴 완료."};
+        return {team : newTeam, player: newPlayer, message: "팀 탈퇴 완료."};
     };
 
 };
--- routes/teams.js
+++ routes/teams.js
@@ -4,25 +4,26 @@ const router = express.Router();
 const TeamsController = require('../controllers/teamsController.js');
 const teamsController = new TeamsController();
 const authMiddleware = require('../middlewares/auth_middleware');
+const wrapAsyncController = require('../middlewares/wrapAsyncController');
 
 
 // 1. 나의 팀 조회
-router.get('/me', authMiddleware, teamsController.getMyTeam)
+router.get('/me', authMiddleware, wrapAsyncController(teamsController.getMyTeam))
 
 // 2. 전체 팀 조회
-router.get('/', teamsController.getAllTeams)
+router.get('/', wrapAsyncController(teamsController.getAllTeams))
 
 // 3. 팀 상세 조회
-router.get('/info', authMiddleware, teamsController.getTeamInfo)
+router.get('/info', authMiddleware, wrapAsyncController(teamsController.getTeamInfo))
 
 // 4. 신규 팀 생성
-router.post('/register', authMiddleware, teamsController.createTeam)
+router.post('/register', authMiddleware, wrapAsyncController(teamsController.createTeam))
 
 // 5. 팀 수정 - 기존 팀 가입
-router.put('/update', authMiddleware, teamsController.updateTeam)
+router.put('/update', authMiddleware, wrapAsyncController(teamsController.updateTeam))
 
 // 6. 팀 수정 - 기존 팀 탈퇴
-router.put('/drop', authMiddleware, teamsController.deleteTeam)
+router.put('/drop', authMiddleware, wrapAsyncController(teamsController.deleteTeam))
 
 
 
