32931747537387344a355701f6d7399691cd1ba8
dntjd960403 <dntjd960403@gmail.com>
2022-11-18 11:42:44 +0900

    11/18 woosung feat 카카오 로그인 구현

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,9 @@
 const express = require("express");
 const router = express.Router();
-const usersRouter = require('./users.js');
-const placesRouter = require('./places.js');
-const teamsRouter = require('./teams.js');
+const usersRouter = require("./users.js");
+const placesRouter = require("./places.js");
+const teamsRouter = require("./teams.js");
+const authRouter = require("./auth");
 //const openPlacesRouter = require('./openPlaces.js');  // open Api연결  안쓸때는 주석해놓기!
 //const mypagesRouter = require('./mypages.js');
 const reservationsRouter = require("./reservations.js");
@@ -12,6 +13,7 @@ const reservationsRouter = require("./reservations.js");
 router.use("/users", usersRouter);
 router.use("/places", placesRouter);
 router.use("/teams", teamsRouter);
+router.use("/auth", authRouter);
 // router.use('/mypages', mypagesRouter);
 router.use("/reservations", reservationsRouter);
 
