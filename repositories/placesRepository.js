d19625707c5499a0bd30aaa788b76e93dfd8650d
dahye24 <rhahsorkddkw@naver.com>
2022-11-24 04:10:46 +0900

    11/24 dahye feat) places 삭제 기능추가

--- repositories/placesRepository.js
+++ repositories/placesRepository.js
@@ -1,169 +1,184 @@
-const { Places, Opens} = require("../models");
+const { Places, Opens, Reservations } = require("../models");
 require("dotenv").config();
-const mysql2 = require('mysql2');  //mysql 모듈 import
+const mysql2 = require("mysql2"); //mysql 모듈 import
 
-var db = mysql2.createConnection({  //mpsql DB 연결  keywords 할때 sequelize 아니라서 다시 연결
+var db = mysql2.createConnection({
+  //mpsql DB 연결  keywords 할때 sequelize 아니라서 다시 연결
   host: process.env.DB_ENDPOINT,
   user: process.env.DB_USERNAME,
   password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME
-   })
+  database: process.env.DB_NAME,
+});
 db.connect();
 
+class PlacesRepository {
+  //시설등록
 
-class PlacesRepository {   //시설등록
+  createPlace = async (
+    loginId,
+    x,
+    y,
+    sports,
+    spotName,
+    spotKind,
+    address,
+    comforts,
+    price,
+    desc,
+    image
+  ) => {
+    const createPlaceData = await Places.create({
+      loginId,
+      x,
+      y,
+      sports,
+      spotName,
+      spotKind,
+      address,
+      comforts,
+      price,
+      desc,
+      image,
+    });
 
-    createPlace = async (loginId,x,y,sports,spotName,spotKind,address,comforts,price,desc,image) => {
+    return createPlaceData;
+  };
 
-      const createPlaceData = await Places.create({
-          loginId,  
-          x,
-          y,
-          sports,
-          spotName,
-          spotKind,
-          address,
-          comforts,
-          price,
-          desc,
-          image
+  findPlaces = async (address) => {
+    //  등록된 시설 주소 찾기
+    const findPlace = await Places.findOne({ where: { address } });
+
+    return findPlace;
+  };
+
+  // 사설 전체 조회
+  findAllPlace = async () => {
+    const places = await Places.findAll();
+
+    return places;
+  };
+
+  // 사설 최신등록 6개만
+  findRecentPlace = async () => {
+    const RecentPlaces = await Places.findAll({
+      limit: 6,
+      order: [["createdAt", "DESC"]],
+    });
+
+    return RecentPlaces;
+  };
+
+  // 사설 + openApi 전체 조회
+  findAllPlaces = async () => {
+    const places = await Places.findAll();
+    const opens = await Opens.findAll();
+
+    return { private: places, public: opens };
+  };
+
+  // 본인이 등록한 시설만 조회
+  findGetPlaces = async (loginId) => {
+    const Place = await Places.findAll({ where: { loginId } });
+
+    return Place;
+  };
+
+  //종목 조회
+  getSports = async (sports) => {
+    const findSports = await Places.findAll({ where: { sports } });
+
+    return findSports;
+  };
+
+  dbQueryAsync = async (sql) => {
+    return new Promise((resolve, reject) => {
+      db.query(sql, (error, result) => {
+        if (error) {
+          reject(error);
+        }
+        resolve(result);
       });
+    });
+  };
 
-      return createPlaceData;
-    };
+  //keyword 조회
+  getKeyword = async (keywords) => {
+    const sql = `SELECT * FROM Places
+      where sports like '%${keywords}%' OR spotName like '%${keywords}%' OR spotKind like '%${keywords}%' OR address like '%${keywords}%' OR comforts like '%${keywords}%' OR price like '%${keywords}%'`;
 
-    findPlaces = async (address) => {  //  등록된 시설 주소 찾기
-      const findPlace = await Places.findOne({where: {address}});
-      
-      return findPlace;
-    };
+    const sql2 = `SELECT * FROM Opens
+      where minclassnm like '%${keywords}%' OR svcstatnm like '%${keywords}%' OR svcnm like '%${keywords}%' OR placenm like '%${keywords}%' OR areanm like '%${keywords}%'`;
 
+    const data = await this.dbQueryAsync(sql);
+    const data2 = await this.dbQueryAsync(sql2);
 
-    // 사설 전체 조회
-    findAllPlace = async () => {
-      const places = await Places.findAll();
+    return { private: data, public: data2 };
+  };
 
-      return places;
-    };
+  // 수정
+  updatePlaces = async (
+    placesId,
+    loginId,
+    x,
+    y,
+    sports,
+    spotName,
+    spotKind,
+    address,
+    comforts,
+    price,
+    desc
+  ) => {
+    const updatePlaces = await Places.update(
+      { x, y, sports, spotName, spotKind, address, comforts, price, desc },
+      { where: { placesId, loginId } }
+    );
+    const findPlaces = await Places.findOne({ where: { placesId } });
+    return findPlaces;
+  };
 
+  // 시설 삭제
+  deletePlaces = async (placesId, loginId) => {
+    const deletePlace = await Places.destroy({ where: { placesId, loginId } });
 
-    // 사설 최신등록 6개만
-    findRecentPlace = async () => {
-      const RecentPlaces = await Places.findAll({ limit: 6, order: [['createdAt', 'DESC']]});
+    return deletePlace;
+  };
 
-      return RecentPlaces;
-    };
+  //placesId 불러오기
+  findPlacesId = async (placesId) => {
+    const findPlaceId = await Places.findByPk(placesId);
 
+    return findPlaceId;
+  };
 
-    // 사설 + openApi 전체 조회
-    findAllPlaces = async () => {
-      const places = await Places.findAll();
-      const opens = await Opens.findAll();
+  //reservation의 place 불러오기
+  findReservationPlace = async (place) => {
+    const findPlace = await Reservations.findOne({ where: { place } });
+    //console.log(findPlace);
+    return findPlace;
+  };
 
-      return {private: places, public: opens};
-    };
+  // open api 전체 조회
+  findAllOpens = async () => {
+    const open = await Opens.findAll();
 
+    return open;
+  };
 
+  // open api 소분류명 조회
+  getSportsOpen = async (minclassnm) => {
+    const findOpenSports = await Opens.findAll({ where: { minclassnm } });
 
-    // 본인이 등록한 시설만 조회
-    findGetPlaces = async (loginId) => {  
-      const Place = await Places.findAll({where: {loginId}});
-  
-      return Place;
-    };
+    return findOpenSports;
+  };
 
-    //종목 조회
-    getSports = async (sports) => {
-      const findSports = await Places.findAll({ where: { sports } });
-  
+  // open api 지역명 조회
+  getRegionOpen = async (areanm) => {
+    const findOpenArea = await Opens.findAll({ where: { areanm } });
+    console.log(findOpenArea);
 
-      return findSports;
-    };
-
-
-
-    dbQueryAsync = async(sql) => {
-      return new Promise((resolve, reject) => {
-        db.query(sql, (error, result) => {
-          if (error) {
-            reject(error);
-          }
-          resolve(result);
-        });
-      });
-    }
-
-    //keyword 조회  
-    getKeyword = async(keywords)=> {
-      
-      const sql = `SELECT * FROM Places
-      where sports like '%${keywords}%' OR spotName like '%${keywords}%' OR spotKind like '%${keywords}%' OR address like '%${keywords}%' OR comforts like '%${keywords}%' OR price like '%${keywords}%'`  
-      
-      const sql2 = `SELECT * FROM Opens
-      where minclassnm like '%${keywords}%' OR svcstatnm like '%${keywords}%' OR svcnm like '%${keywords}%' OR placenm like '%${keywords}%' OR areanm like '%${keywords}%'`
-      
-      const data = await this.dbQueryAsync(sql);
-      const data2 = await this.dbQueryAsync(sql2);
-      
-      return {private: data, public: data2};
-
-      
-    }
-    
-
-
-    // 수정
-    updatePlaces = async (placesId,loginId,x,y,sports,spotName,spotKind,address,comforts,price,desc) => {
-      const updatePlaces = await Places.update(
-          {x,y,sports,spotName,spotKind,address,comforts,price,desc}, {where: {placesId, loginId}}
-      );
-      const findPlaces = await Places.findOne({where : {placesId}});
-      return findPlaces;
-    };
-
-    // 시설 삭제
-    deletePlaces = async (placesId, loginId) => {
-      const deletePlace = await Places.destroy({where: {placesId, loginId}});
-      
-      return deletePlace;
-    };
-
-    // //placesId 불러오기
-    findPlacesId = async (placesId) => { 
-      const findPlaceId = await Places.findByPk(placesId);
-
-      return findPlaceId;
-    };
-
-
-
-
-
-
-
-    // open api 전체 조회
-    findAllOpens = async () => {
-      const open = await Opens.findAll();
-
-      return open;
-    };
-
-
-    // open api 소분류명 조회
-    getSportsOpen = async (minclassnm) => {
-      const findOpenSports = await Opens.findAll({ where: { minclassnm } });
-
-      return findOpenSports;
-    };
-
-    // open api 지역명 조회
-    getRegionOpen = async (areanm) => {
-      const findOpenArea = await Opens.findAll({ where: { areanm } });
-      console.log(findOpenArea);
-
-      return findOpenArea;
-    };
-
+    return findOpenArea;
+  };
 }
 
 module.exports = PlacesRepository;
--- services/placesService.js
+++ services/placesService.js
@@ -4,44 +4,59 @@ class PlacesService {
   placesRepository = new PlacesRepository();
 
   // 시설등록
-  createPlace = async (loginId,x,y,sports,spotName,spotKind,address,comforts,price,desc,image) => {
-
+  createPlace = async (
+    loginId,
+    x,
+    y,
+    sports,
+    spotName,
+    spotKind,
+    address,
+    comforts,
+    price,
+    desc,
+    image
+  ) => {
     const findPlaces = await this.placesRepository.findPlaces(address);
-    
 
-        if (!sports || !spotName || !spotKind || !address || !comforts || !price || !desc) {
-          const err = new Error(`placesService Error`);
-          err.statusCode = 400;
-          err.message = '빈칸을 입력해주세요.';
-          throw err;
-        };
+    if (
+      !sports ||
+      !spotName ||
+      !spotKind ||
+      !address ||
+      !comforts ||
+      !price ||
+      !desc
+    ) {
+      const err = new Error(`placesService Error`);
+      err.statusCode = 400;
+      err.message = "빈칸을 입력해주세요.";
+      throw err;
+    }
 
-        if (findPlaces) {
-          const err = new Error(`placesService Error`);
-          err.statusCode = 400;
-          err.message = '이미 등록된 시설입니다.';
-          throw err;
-             
-        };
+    if (findPlaces) {
+      const err = new Error(`placesService Error`);
+      err.statusCode = 400;
+      err.message = "이미 등록된 시설입니다.";
+      throw err;
+    }
 
-        const createPlaces = await this.placesRepository.createPlace(
-          loginId,
-          x,
-          y,
-          sports,
-          spotName,
-          spotKind,
-          address,
-          comforts,
-          price,
-          desc,
-          image
-        );
+    const createPlaces = await this.placesRepository.createPlace(
+      loginId,
+      x,
+      y,
+      sports,
+      spotName,
+      spotKind,
+      address,
+      comforts,
+      price,
+      desc,
+      image
+    );
 
-        return createPlaces;
+    return createPlaces;
   };
-
-
 
   findAllPlace = async () => {
     // 사설 전체 조회
@@ -51,17 +66,14 @@ class PlacesService {
     return findAllPlace;
   };
 
-
   findRecentPlace = async () => {
-   // 사설 최신등록 6개만
+    // 사설 최신등록 6개만
 
     const findRecentPlace = await this.placesRepository.findRecentPlace();
 
     return findRecentPlace;
   };
 
-
-  
   findAllPlaces = async () => {
     // 사설 + openApi 전체 조회
 
@@ -73,13 +85,12 @@ class PlacesService {
   findGetPlaces = async (loginId) => {
     // 본인이 등록한 시설만 조회
 
-    
     const findPlaces = await this.placesRepository.findGetPlaces(loginId);
 
-    if(findPlaces.length == 0){
+    if (findPlaces.length == 0) {
       const err = new Error(`placesService Error`);
       err.statusCode = 404;
-      err.message = '등록된 시설이 없습니다.';
+      err.message = "등록된 시설이 없습니다.";
       throw err;
     }
 
@@ -88,114 +99,122 @@ class PlacesService {
 
   getSports = async (sports) => {
     //종목별 조회
-  
+
     const findFootsal = await this.placesRepository.getSports(sports);
 
-    if(findFootsal.length === 0){
+    if (findFootsal.length === 0) {
       const err = new Error(`placesService Error`);
       err.statusCode = 404;
-      err.message = '등록이 안된 종목입니다.';
+      err.message = "등록이 안된 종목입니다.";
       throw err;
     }
 
     return findFootsal;
-
   };
-
-
-
-
-
 
   getKeyword = async (keywords) => {
     //키워드별 조회
-  
+
     const findKeyword = await this.placesRepository.getKeyword(keywords);
-  
-    if(findKeyword.private.length === 0 && findKeyword.public.length  === 0){
+
+    if (findKeyword.private.length === 0 && findKeyword.public.length === 0) {
       const err = new Error(`placesService Error`);
       err.statusCode = 404;
-      err.message = '존재하지 않는 키워드입니다.';
+      err.message = "존재하지 않는 키워드입니다.";
       throw err;
     }
 
     return findKeyword;
-
   };
 
-
-
-
-
-
-
-   // 수정
-  updatePlaces = async (placesId,loginId,x,y,sports,spotName,spotKind,address,comforts,price,desc) => {
+  // 수정
+  updatePlaces = async (
+    placesId,
+    loginId,
+    x,
+    y,
+    sports,
+    spotName,
+    spotKind,
+    address,
+    comforts,
+    price,
+    desc
+  ) => {
     const findplacesId = await this.placesRepository.findPlacesId(placesId);
-  
-       
-          // 수정할 시설 자체가 없을때
-          if (findplacesId === null) {
-            const err = new Error(`placesService Error`);
-            err.statusCode = 404;
-            err.message = '수정할 시설이 없습니다.';
-            throw err;
-            
-        }
 
-        //시설을 등록한 유저 아이디와 지금 정보를 바꾸려는 유저 아이디가 다른사람일때
-        if (loginId !== findplacesId.dataValues.loginId) {
-          const err = new Error(`placesService Error`);
-            err.statusCode = 403;
-            err.message = '시설 정보를 수정할 권한이 없습니다.';
-            throw err;
-          };
+    // 수정할 시설 자체가 없을때
+    if (findplacesId === null) {
+      const err = new Error(`placesService Error`);
+      err.statusCode = 404;
+      err.message = "수정할 시설이 없습니다.";
+      throw err;
+    }
 
+    //시설을 등록한 유저 아이디와 지금 정보를 바꾸려는 유저 아이디가 다른사람일때
+    if (loginId !== findplacesId.dataValues.loginId) {
+      const err = new Error(`placesService Error`);
+      err.statusCode = 403;
+      err.message = "시설 정보를 수정할 권한이 없습니다.";
+      throw err;
+    }
 
-        const updateData = await this.placesRepository.updatePlaces(placesId,loginId,x,y,sports,spotName,spotKind,address,comforts,price,desc);
-          
+    const updateData = await this.placesRepository.updatePlaces(
+      placesId,
+      loginId,
+      x,
+      y,
+      sports,
+      spotName,
+      spotKind,
+      address,
+      comforts,
+      price,
+      desc
+    );
 
-        return updateData;
-          
-        
+    return updateData;
   };
 
   //삭제
-  deletePlaces = async (placesId, loginId) => { 
-
-  
+  deletePlaces = async (placesId, loginId) => {
     const findPlacesId = await this.placesRepository.findPlacesId(placesId); //repository에서 placesId 불러오기
+    const place = findPlacesId.spotName;
 
-        //해당하는 데이터가 없으면
-        if (findPlacesId === null) {
-          const err = new Error(`placesService Error`);
-            err.statusCode = 404;
-            err.message = '삭제할 시설이 없습니다.';
-            throw err;
-          };
-          
+    const findReservationPlace =
+      await this.placesRepository.findReservationPlace(place); //repository에서 reservation의 place 불러오기
 
-        //댓글을 삭제하려는 userId와 댓글을 작성한 userId가 다를때
-        if (loginId !== findPlacesId.dataValues.loginId) {
-          const err = new Error(`placesService Error`);
-            err.statusCode = 403;
-            err.message = '시설을 삭제할 권한이 없습니다.';
-            throw err;
-        };
-            
-        const deleteData = await this.placesRepository.deletePlaces(placesId, loginId);
-  
-          return deleteData;
+    //해당하는 데이터가 없으면
+    if (findPlacesId === null) {
+      const err = new Error(`placesService Error`);
+      err.statusCode = 404;
+      err.message = "삭제할 시설이 없습니다.";
+      throw err;
+    }
+
+    //예약 된 구장은 삭제 x
+    if (findReservationPlace) {
+      const err = new Error(`placesService Error`);
+      err.statusCode = 202;
+      err.message = "예약이 있는 구장은 삭제할 수 없습니다.";
+      throw err;
+    }
+
+    //댓글을 삭제하려는 userId와 댓글을 작성한 userId가 다를때
+    if (loginId !== findPlacesId.dataValues.loginId) {
+      const err = new Error(`placesService Error`);
+      err.statusCode = 403;
+      err.message = "시설을 삭제할 권한이 없습니다.";
+      throw err;
+    }
+
+    const deleteData = await this.placesRepository.deletePlaces(
+      placesId,
+      loginId
+    );
+
+    return deleteData;
   };
-     
-
-    
-
-
-
-
-
-
 
   findAllOpens = async () => {
     // open api 전체조회
@@ -205,24 +224,21 @@ class PlacesService {
     return findOpenPlace;
   };
 
-
-
-
-
   getSportsOpen = async (minclassnm) => {
     //소분류명 open api 조회
-    
-    const findOpenSports = await this.placesRepository.getSportsOpen(minclassnm);
 
-    if(findOpenSports.length === 0){
+    const findOpenSports = await this.placesRepository.getSportsOpen(
+      minclassnm
+    );
+
+    if (findOpenSports.length === 0) {
       const err = new Error(`placesService Error`);
       err.statusCode = 404;
-      err.message = '등록이 안된 종목입니다.';
+      err.message = "등록이 안된 종목입니다.";
       throw err;
     }
 
     return findOpenSports;
-    
   };
 
   getRegionOpen = async (areanm) => {
@@ -230,15 +246,14 @@ class PlacesService {
 
     const findOpenArea = await this.placesRepository.getRegionOpen(areanm);
 
-    if(findOpenArea.length === 0){
+    if (findOpenArea.length === 0) {
       const err = new Error(`placesService Error`);
       err.statusCode = 404;
-      err.message = '등록이 안된 지역입니다.';
+      err.message = "등록이 안된 지역입니다.";
       throw err;
     }
 
     return findOpenArea;
-    
   };
 }
 
