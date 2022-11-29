0fc702bbc330b4868449844ac7bdd123dc88ea14
Thorjuri <sjl1992@naver.com>
2022-11-29 16:36:30 +0900

    11/29 서주리 mail 수정

--- mail.js
+++ mail.js
@@ -2,20 +2,20 @@ const nodemailer = require('nodemailer');
 require("dotenv").config();
 
 const sendEmail = (email, contents, teamName)=> {
-    var transporter = nodemailer.createTransport({
-        service: process.env.SERVICE,   // 메일 보내는 곳
-        port: 465,
-        host: process.env.HOST,  
-        secure: true,  //true 시 SSL 인증된 https 통신 필요
-        requireTLS: true ,
-        auth: {
-          user: process.env.MAIL_ID,  // 보내는 메일의 주소
-          pass: process.env.MAIL_PASS   // 보내는 메일의 비밀번호
-        }
+  var transporter = nodemailer.createTransport({
+      service: process.env.SERVICE,   // 메일 보내는 곳
+    // port: 587,
+    // host: process.env.HOST,  
+    // secure: false,  //true 시 SSL 인증된 https 통신 필요
+    // requireTLS: true ,
+      auth: {
+        user: process.env.MAIL_ID,  // 보내는 메일의 주소
+        pass: process.env.MAIL_PASS   // 보내는 메일의 비밀번호
+    }
         });
       // 메일 옵션
     var mailOptions = {
-        from: process.env.MAIL, // 보내는 메일의 주소
+        from: process.env.MAIL_ID, // 보내는 메일의 주소
         to: email, // 수신할 이메일
         subject: "SPOTS 매칭 예약 내역 안내", // 메일 제목
         text: `🏸안녕하세요! 생활 체육시설 매칭 서비스, SPOTS 입니다!⚽ 
