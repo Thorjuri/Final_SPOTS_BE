e5052a7b842c5d815369860608b334a305c9fbfe
Thorjuri <sjl1992@naver.com>
2022-12-01 22:34:03 +0900

    12/1 서주리 chat on_chat/start_chat 이벤트 채널 추가

--- app.js
+++ app.js
@@ -61,7 +61,7 @@ app.use(errorHandlerMiddleware);
 // });
 
 // public room 목록 추출
-function publicRooms(){ //객체의 구조 분해 할당 3겹 (io란 객체 안의, sockets 객체 안의, adapter 객체 안의 sids, rooms 란 객체)
+function publicRooms(){ 
   const {
       sockets: {
           adapter: { sids, rooms }
@@ -102,49 +102,54 @@ function findRoom(room){
     }
   });
   return roomid;
-}
+};
 
 // room의 접속자 수 추출
 function countRoom(roomName){
   return io.sockets.adapter.rooms.get(roomName)?.size;
-}
-
+};
 
 io.on("connection", socket => {
-  const roomName = socket.id
-  socket.join(roomName)
+  const roomName = socket.id;
+  socket.join(roomName);
   const rooms = findUser();
-  rooms.splice(rooms.indexOf(roomName),1)
+  rooms.splice(rooms.indexOf(roomName),1);
 
-  socket.emit("client_main", roomName)
-
-  socket.emit("admin_roomlist", rooms)
+  socket.emit("client_main", roomName);
+  socket.emit("admin_roomlist", rooms);
+  socket.on("on_chat", (obj)=> {
+    const convert = JSON.parse(obj);
+    const {roomName, nickname} = convert;
+    const message = '상담이 곧 시작됩니다. 잠시만 기다려주세요.'
+    const data = { roomName, nickname, message };
+    io.sockets.in(roomName).emit("start_chat", data);
+  });
 
   socket.on("admin_enter_room", room => {
-    socket.join(room)
-    const roomName = room
-    const nickname = "admin"
+    socket.join(room);
+    const roomName = room;
+    const nickname = "admin";
     const message = "안녕하세요 고객님, 생활체육 매칭 서비스 SPOTS입니다. 무엇을 도와드릴까요?";
-    const data = { roomName, nickname, message}
+    const data = { roomName, nickname, message};
     io.sockets.in(roomName).emit("new_message", data);
   });
 
   socket.on("chatting", (obj) => {
-    const convert = JSON.parse(obj)
+    const convert = JSON.parse(obj);
     const {roomName, nickname, value} = convert;
-    const data = {roomName, nickname, message:value}
+    const data = {roomName, nickname, message:value};
     io.sockets.in(roomName).emit("new_message", data);
-  })
+  });
 
   socket.on("disconnecting", () => { //disconnecting = 연결 종료 직전**
-      const nickname = socket.id.slice(0,5)
-      const message = `${nickname} 님이 퇴장하셨습니다.`  
-      const roomName = socket.id
-      io.sockets.in(roomName).emit("left_notice", message) // 연결 종료 시(직전에) 해당 room 전체에 메세지 
+      const nickname = socket.id.slice(0,5);
+      const message = `${nickname} 님이 퇴장하셨습니다.`;
+      const roomName = socket.id;
+      io.sockets.in(roomName).emit("left_notice", message); // 연결 종료 시(직전에) 해당 room 전체에 메세지 
       });
 
   socket.on("disconnect", ()=>{ //disconnecting = 연결 종료 직후**
-    console.log(socket.rooms)
+    console.log(socket.rooms);
   });
 
 });
