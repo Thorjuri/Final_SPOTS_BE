d8de4a6cca0fd499d0c193ecbceae061a0eb97fb
dntjd960403 <dntjd960403@gmail.com>
2022-12-02 15:02:56 +0900

    12/2 woosung fix redis on

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,9 @@ const router = express.Router();
 const usersRouter = require("./users.js");
 const placesRouter = require("./places.js");
 const teamsRouter = require("./teams.js");
-const authRouter = require("./auth");
+const kakaoRouter = require("./auth/kakao");
+const googleRouter = require("./auth/google");
+const naverRouter = require("./auth/naver");
 //const openPlacesRouter = require('./openPlaces.js');  // open Api연결  안쓸때는 주석해놓기!
 //const mypagesRouter = require('./mypages.js');
 const reservationsRouter = require("./reservations.js");
@@ -13,7 +15,7 @@ const reservationsRouter = require("./reservations.js");
 router.use("/users", usersRouter);
 router.use("/places", placesRouter);
 router.use("/teams", teamsRouter);
-router.use("/auth", authRouter);
+router.use("/auth", kakaoRouter); // 수정해야 됨
 // router.use('/mypages', mypagesRouter);
 router.use("/reservations", reservationsRouter);
 
