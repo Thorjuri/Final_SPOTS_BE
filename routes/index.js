4ee6b0963cfd12305e2e406b97c9af01715be3d7
dntjd960403 <dntjd960403@gmail.com>
2022-12-06 00:56:10 +0900

    12/6 woosung fix 소셜 레이어드 적용

--- routes/index.js
+++ routes/index.js
@@ -3,9 +3,7 @@ const router = express.Router();
 const usersRouter = require("./users.js");
 const placesRouter = require("./places.js");
 const teamsRouter = require("./teams.js");
-const kakaoRouter = require("./auth/kakao");
-const googleRouter = require("./auth/google");
-const naverRouter = require("./auth/naver");
+const authRouter = require("./auth");
 //const openPlacesRouter = require('./openPlaces.js');  // open Api연결  안쓸때는 주석해놓기!
 //const mypagesRouter = require('./mypages.js');
 const reservationsRouter = require("./reservations.js");
@@ -15,8 +13,7 @@ const reservationsRouter = require("./reservations.js");
 router.use("/users", usersRouter);
 router.use("/places", placesRouter);
 router.use("/teams", teamsRouter);
-router.use("/kakao", kakaoRouter);
-router.use("/google", googleRouter);
+router.use("/auth", authRouter);
 // router.use('/mypages', mypagesRouter);
 router.use("/reservations", reservationsRouter);
 
