9aceaa89210bc0f5d2f3607046c3eaff3f0acf45
dntjd960403 <dntjd960403@gmail.com>
2022-12-10 18:20:03 +0900

    12/10 woosung fix 소셜  회원탈퇴, 탈퇴 취소

--- repositories/authRepository.js
+++ repositories/authRepository.js
@@ -2,7 +2,7 @@ const { Users } = require("../models");
 
 class AuthRepository {
   findUser = async (loginId) => {
-    const user = await Users.findOne({ where: { loginId } });
+    const user = await Users.findOne({ where: { loginId }, paranoid: false });
     return user;
   };
 
@@ -17,12 +17,13 @@ class AuthRepository {
   };
 
   getAccKey = async (loginId, accKey) => {
-    const getAccKey = await Users.update({ accKey:accKey }, { where: { loginId: loginId } });
-    return getAccKey
-  }
+    const getAccKey = await Users.update({ accKey: accKey }, { where: { loginId: loginId } });
+    return getAccKey;
+  };
   signup = async (loginId, password, nickname, gender, phone, sports, favSports, profileImg) => {
-    if (!profileImg) profileImg =
-      "https://woosungbucket.s3.ap-northeast-2.amazonaws.com/original/1669128469071_spots2.png";
+    if (!profileImg)
+      profileImg =
+        "https://woosungbucket.s3.ap-northeast-2.amazonaws.com/original/1669128469071_spots2.png";
     if (!sports) sports = [];
     if (!favSports) favSports = [];
     const point = 300000;
