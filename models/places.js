1b42bb065fc94a9d8f3438b5a2be08d4d1ea2b87
dntjd960403 <dntjd960403@gmail.com>
2022-12-12 22:34:00 +0900

    12/12 woosung feat 테이블 관계 설정

--- models/places.js
+++ models/places.js
@@ -1,75 +1,85 @@
-'use strict';
-const { Model } = require('sequelize');
+"use strict";
+const { Model } = require("sequelize");
 module.exports = (sequelize, DataTypes) => {
-    class Places extends Model {
-        /**
-         * Helper method for defining associations.
-         * This method is not a part of Sequelize lifecycle.
-         * The `models/index` file will call this method automatically.
-         */
-        static associate(models) {
-        }
+  class Places extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      this.belongsTo(models.Users, { foreignKey: "loginId" });
+      this.hasMany(models.Reservations, {
+        as: "Reservations",
+        foreignKey: "spotName",
+      });
     }
-    Places.init(
-        {
-            placesId: {
-                primaryKey: true,
-                autoIncrement: true,
-                type: DataTypes.INTEGER,
-            },
-            loginId: {
-                type: DataTypes.STRING,
-                allowNull: false,
-            },
-            x: {
-                type: DataTypes.STRING,
-                allowNull: false,
-            },   
-            y: {
-                type: DataTypes.STRING,
-                allowNull: false,
-            },
-            image: {
-                type: DataTypes.STRING,
-                allowNull: true,
-            },
-            sports: {
-                type: DataTypes.STRING,
-                allowNull: false,
-            },    
-            spotName: {
-                type: DataTypes.STRING,
-                allowNull: false,
-            },
-            spotKind: {
-                type: DataTypes.STRING,
-                allowNull: false,
-            },
-            address: {
-                type: DataTypes.STRING,
-                allowNull: false,
-            },
-            comforts: {
-                type: DataTypes.JSON,
-                allowNull: false,
-                defaultValue: {"comforts" : []}
-            },
-            price: {
-                type: DataTypes.INTEGER,
-                allowNull: false,
-            },
-            desc: {
-                type: DataTypes.STRING,
-                allowNull: true,
-                defaultValue: "설명을 적어주세요."
-            }
+  }
+  Places.init(
+    {
+      placesId: {
+        primaryKey: true,
+        autoIncrement: true,
+        type: DataTypes.INTEGER,
+      },
+      loginId: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        references: {
+          model: "Users",
+          key: "loginId",
         },
-        {
-            sequelize,
-            modelName: 'Places',
-            timestamps: true,
-            paranoid: true,
-        }
-    );
-    return Places;
+        onDelete: "cascade",
+      },
+      x: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      y: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      image: {
+        type: DataTypes.STRING,
+        allowNull: true,
+      },
+      sports: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      spotName: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      spotKind: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      address: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      comforts: {
+        type: DataTypes.JSON,
+        allowNull: false,
+        defaultValue: { comforts: [] },
+      },
+      price: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+      },
+      desc: {
+        type: DataTypes.STRING,
+        allowNull: true,
+        defaultValue: "설명을 적어주세요.",
+      },
+    },
+    {
+      sequelize,
+      modelName: "Places",
+      timestamps: true,
+      paranoid: true,
+    }
+  );
+  return Places;
 };
--- models/reservations.js
+++ models/reservations.js
@@ -1,74 +1,86 @@
-'use strict';
-const { Model } = require('sequelize');
+"use strict";
+const { Model } = require("sequelize");
 module.exports = (sequelize, DataTypes) => {
-    class Reservations extends Model {
-        /**
-         * Helper method for defining associations.
-         * This method is not a part of Sequelize lifecycle.
-         * The `models/index` file will call this method automatically.
-         */
-        static associate(models) {
-            // Reservations.hasOne(models.Places, {
-            //     foreignKey: 'place',
-            //     targetKey: 'place',
-            // });
-            // Reservations.hasOne(models.Teams, {
-            //     foreignKey: 'teamName',
-            //     targetKey: 'teamName',
-            // });
-        }
+  class Reservations extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // Reservations.hasOne(models.Places, {
+      //     foreignKey: 'place',
+      //     targetKey: 'place',
+      // });
+      // Reservations.hasOne(models.Teams, {
+      //     foreignKey: 'teamName',
+      //     targetKey: 'teamName',
+      // });
+      this.belongsTo(models.Places, { foreignKey: "spotName" });
+      this.belongsTo(models.Teams, { foreignKey: "admin" });
     }
-    Reservations.init(
-        {
-            reservationId: {
-                primaryKey: true,
-                autoIncrement: true,
-                type: DataTypes.INTEGER,
-            },
-            matchId: {
-                type: DataTypes.STRING,
-                allowNull: false,
-            },
-            place: {
-                type: DataTypes.STRING,
-                allowNull: false,
-            },
-            isDouble: {
-                type: DataTypes.BOOLEAN,
-                allowNull: true,
-            },
-            teamName: {
-                type: DataTypes.STRING,
-                allowNull: false,
-            },
-            member: {
-                type: DataTypes.INTEGER,
-                allowNull: false,
-            },
-            date: {
-                type: DataTypes.STRING,
-                allowNull: false,
-            },
-            admin: {
-                type: DataTypes.STRING,
-                allowNull: false,
-            },
-            price: {
-                type: DataTypes.INTEGER,
-                allowNull: false,
-            },
-            result: {
-                type: DataTypes.STRING,
-                allowNull: true,
-                defaultValue: "매칭 전"
-            }
+  }
+  Reservations.init(
+    {
+      reservationId: {
+        primaryKey: true,
+        autoIncrement: true,
+        type: DataTypes.INTEGER,
+      },
+      matchId: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      place: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        // references: {
+        //   model: "Places",
+        //   key: "spotName",
+        // },
+        // onDelete: "cascade",
+      },
+      isDouble: {
+        type: DataTypes.BOOLEAN,
+        allowNull: true,
+      },
+      teamName: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      member: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+      },
+      date: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      admin: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        references: {
+          model: "Teams",
+          key: "admin",
         },
-        {
-            sequelize,
-            modelName: 'Reservations',
-            timestamps: true,
-            paranoid: true,
-        }
-    );
-    return Reservations;
+        onDelete: "cascade",
+      },
+      price: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+      },
+      result: {
+        type: DataTypes.STRING,
+        allowNull: true,
+        defaultValue: "매칭 전",
+      },
+    },
+    {
+      sequelize,
+      modelName: "Reservations",
+      timestamps: true,
+      paranoid: true,
+    }
+  );
+  return Reservations;
 };
